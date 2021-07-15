8f95c8c536f622b89619fd03b09f15e7c253471a
Elias Bianchi <elias.bianchi@utec.edu.uy>
2021-07-15 17:29:25 -0300

    fondo y cards

--- test/js/script.js
+++ test/js/script.js
@@ -1,66 +1,117 @@
-/*------------------------------------------------TRAER LISTA DE IMAGENES------------------------------------------------*/
+//------------------------------------------------TRAER LISTA DE PUBLICACIONES------------------------------------------------
 
-let lista = document.querySelector(".leftcolumn");
-fetch("IndexPhoto.json")
-    .then(response => response.json())
-    .then(data => {
-        data.reverse();
+function listarPublicaciones() {
 
-
-        /*----------------------------ULTIMA PUBLICACION----------------------------*/
-        for (let index = 0; index < 2; index++) {
-            //console.log(data[index]);
-            let uP = document.getElementById('ultimasPublicaciones');
-            let div = document.createElement('div');
-            let uImg = document.createElement('img');
-            let uTi = document.createElement('h4');
-
-            let ultima = data[index];
-            let fotosLength = Object.keys(ultima.fotos).length; //OBTIENE LA CANTIDAD DE FOTOS
-            //console.log(fotosLength);
-            let fotoPrincipal = ultima.fotos[fotosLength - 1]; //OBTIENE LA ULTIMA FOTO
-            //console.log(fotosLength + ": " + fotoPrincipal);
-
-            uTi.innerText = ultima.nombre;
-            uImg.src = fotoPrincipal;
-
-            div.appendChild(uImg);
-            div.append(uTi)
-            uP.appendChild(div);
-        }
+    let lista = document.querySelector(".leftcolumn");
+    fetch("IndexPhoto.json")
+        .then(response => response.json())
+        .then(data => {
+            data.reverse();
 
 
-        for (const todo of data) {
-            let { nombre, descripcion, fechas, fotos } = todo;
+            //----------------------------ULTIMAS PUBLICACIONES----------------------------
+            for (let index = 0; index < 2; index++) {
+                //console.log(data[index]);
+                let uP = document.getElementById('ultimasPublicaciones');
+                let div = document.createElement('div');
+                let uImg = document.createElement('img');
+                let uTi = document.createElement('h4');
 
-            let fotosLength = Object.keys(todo.fotos).length; //OBTIENE LA CANTIDAD DE FOTOS
-            //console.log(fotosLength);
-            let fotoPrincipal = fotos[fotosLength - 1]; //OBTIENE LA ULTIMA FOTO
-            //console.log(fotoPrincipal);
-            /*for (let img in fotos) { //LISTA TODOS LOS ELEMENTOS DE FOTO
-                console.log(fotos[img]);
-            }*/
+                let ultima = data[index];
+                let Cantfotos = Object.keys(ultima.fotos).length; //OBTIENE LA CANTIDAD DE FOTOS
+                //console.log(Cantfotos);
+                let fotoPrincipal = ultima.fotos[Cantfotos - 1]; //OBTIENE LA ULTIMA FOTO
+                //console.log(Cantfotos + ": " + fotoPrincipal);
 
-            let card = document.createElement('div');
-            let imagen = document.createElement('img');
-            let info = document.createElement('div');
-            let titulo = document.createElement('h2');
-            let fech = document.createElement('h5');
-            let desc = document.createElement('p');
+                uTi.innerText = ultima.nombre;
+                uImg.src = fotoPrincipal;
 
-            card.className = "card";
-            imagen.src = fotoPrincipal;
+                div.addEventListener('click', function() {
+                    abrirImagen(ultima);
+                });
 
-            info.className = "info";
-            titulo.innerText = `${nombre}`
-            desc.innerText = `${descripcion}`
-            fech.innerText = `${fechas}`
+                div.appendChild(uImg);
+                div.append(uTi)
+                uP.appendChild(div);
+            }
 
-            card.append(imagen);
-            info.append(titulo);
-            info.append(fech);
-            info.append(desc);
-            card.append(info);
-            lista.append(card);
-        }
-    });
+            //----------------------------TODAS LAS PUBLICACIONES----------------------------
+            for (const todo of data) {
+                let { nombre, descripcion, fechas, fotos } = todo;
+
+                let fotosLength = Object.keys(todo.fotos).length; //OBTIENE LA CANTIDAD DE FOTOS
+                //console.log(fotosLength);
+                let fotoPrincipal = fotos[fotosLength - 1]; //OBTIENE LA ULTIMA FOTO
+                //console.log(fotoPrincipal);
+                /*for (let img in fotos) { //LISTA TODOS LOS ELEMENTOS DE FOTO
+                    console.log(fotos[img]);
+                }*/
+
+                let card = document.createElement('div');
+                let imagen = document.createElement('img');
+                let content = document.createElement('div');
+                let titulo = document.createElement('h2');
+                let fech = document.createElement('h5');
+                let desc = document.createElement('p');
+
+                card.classList.add("card");
+                card.addEventListener('click', function() {
+                    abrirImagen(todo);
+                });
+
+                imagen.src = fotoPrincipal;
+
+                content.className = "content";
+                titulo.innerText = `${nombre}`
+                desc.innerText = `${descripcion}`
+                fech.innerText = `${fechas}`
+
+                content.append(titulo);
+                content.append(imagen);
+                content.append(fech);
+                content.append(desc);
+                card.append(content);
+                lista.append(card);
+            }
+        });
+}
+
+//------------------------------------------------ABRIR IMAGEN------------------------------------------------
+function abrirImagen(obj) {
+    //console.log(obj);
+
+    const cards = document.querySelectorAll(".leftcolumn .card");
+    //console.log(cards);
+
+    //LIMPIO LA PANTALLA PRINCIPAL
+    //console.log(cards.length);
+    for (let index = 0; index < cards.length; index++) {
+        //console.log(cards[index]);
+        cards[index].style.display = 'none';
+    }
+
+    const lista = document.querySelector(".leftcolumn");
+    const card = document.createElement('div')
+    let Cantfotos = Object.keys(obj.fotos).length; //OBTIENE LA CANTIDAD DE FOTOS
+    //console.log(Cantfotos);
+
+    card.classList.toggle('card');
+
+    for (let index = 0; index < Cantfotos - 1; index++) {
+        let img = document.createElement('img');
+
+        img.src = obj.fotos[index];
+
+        card.append(img);
+    }
+    lista.append(card);
+};
+
+//------------------------------------------------MENU------------------------------------------------
+document.getElementById('menutoggle').addEventListener('click', function(evt) {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+});
+
+
+listarPublicaciones();
